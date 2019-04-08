8129ac606d10af4150826036bc35866939eee6c6
umasaki1182 <40985368+umasaki1182@users.noreply.github.com>
2019-04-08 15:49:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
 'use strict';
+
+let localStream = null;
+let peer = null;
+let existingCall = null;
+
+navigator.mediaDevices.getDisplayMedia({'video':true})
+    .then(function (stream) {
+        // Success
+        $('#my-video').get(0).srcObject = stream;
+        localStream = stream;
+    }).catch(function (error) {
+        // Error
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
+
+peer = new Peer({
+    key: 'dac55d92-613e-4565-a476-72c338ff8759',
+    debug: 3
+});
+
+peer.on('open', function(){
+    $('#my-id').text(peer.id);
+});
+peer.on('call', function(call){
+    call.answer(localStream);
+    setupCallEventHandlers(call);
+});
+peer.on('error', function(err){
+    alert(err.message);
+});
+peer.on('close', function(){
+});
+peer.on('disconnected', function(){
+});
+
+$('#make-call').submit(function(e){
+    e.preventDefault();
+    const call = peer.call($('#callto-id').val(), localStream);
+    setupCallEventHandlers(call);
+});
+
+$('#end-call').click(function(){
+    existingCall.close();
+});
+
+function setupCallEventHandlers(call){
+    if (existingCall) {
+        existingCall.close();
+    };
+
+    existingCall = call;
+
+    call.on('stream', function(stream){
+        addVideo(call,stream);
+        setupEndCallUI();
+        $('#their-id').text(call.remoteId);
+    });
+
+    call.on('close', function(){
+        removeVideo(call.remoteId);
+        setupMakeCallUI();
+    });
+}
+
+function addVideo(call,stream){
+    $('#their-video').get(0).srcObject = stream;
+}
+
+function removeVideo(peerId){
+    $('#their-video').get(0).srcObject = undefined;
+}
+
+function setupMakeCallUI(){
+    $('#make-call').show();
+    $('#end-call').hide();
+}
+
+function setupEndCallUI() {
+    $('#make-call').hide();
+    $('#end-call').show();
+}
